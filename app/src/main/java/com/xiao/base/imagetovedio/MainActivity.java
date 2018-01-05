55fe01c83432a9b3fb5563918a09ea59ff2328ed
1612174714@qq.com <xiao10034263>
2018-01-05 10:20:35 +0800

    Initial commit

---
+++ app/src/main/java/com/xiao/base/imagetovedio/MainActivity.java
@@ -0,0 +1,220 @@
+package com.xiao.base.imagetovedio;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSION_REQUEST_CODE = 333;
+    int[] images = {
+            R.mipmap.m01,
+            R.mipmap.m02,
+            R.mipmap.m03,
+            R.mipmap.m04,
+            R.mipmap.m05,
+            R.mipmap.m06,
+
+            R.mipmap.m01,
+            R.mipmap.m02,
+            R.mipmap.m03,
+            R.mipmap.m04,
+            R.mipmap.m05,
+            R.mipmap.m06,
+
+            R.mipmap.m01,
+            R.mipmap.m02,
+            R.mipmap.m03,
+            R.mipmap.m04,
+            R.mipmap.m05,
+            R.mipmap.m06,
+
+            R.mipmap.m01,
+            R.mipmap.m02,
+            R.mipmap.m03,
+            R.mipmap.m04,
+            R.mipmap.m05,
+            R.mipmap.m06,
+
+            R.mipmap.m01,
+            R.mipmap.m02,
+            R.mipmap.m03,
+            R.mipmap.m04,
+            R.mipmap.m05,
+            R.mipmap.m06
+
+
+    };
+    private static final File OUTPUT_DIR = Environment.getExternalStorageDirectory();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+         * 第 1 步: 检查是否有相应的权限
+         */
+        boolean isAllGranted = checkPermissionAllGranted(
+                new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                }
+        );
+        // 如果这3个权限全都拥有, 则直接执行备份代码
+        if (isAllGranted) {
+            doBackup();
+            return;
+        }
+
+/**
+ * 第 2 步: 请求权限
+ */
+        // 一次请求多个权限, 如果其他有权限是已经授予的将会自动忽略掉
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                },
+                MY_PERMISSION_REQUEST_CODE
+        );
+
+
+    }
+
+    /**
+     * 第 3 步: 申请权限结果返回处理
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+            boolean isAllGranted = true;
+
+            // 判断是否所有的权限都已经授予了
+            for (int grant : grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false;
+                    break;
+                }
+            }
+
+            if (isAllGranted) {
+                // 如果所有的权限都授予了, 则执行备份代码
+                doBackup();
+
+            } else {
+                // 弹出对话框告诉用户需要权限的原因, 并引导用户去应用权限管理中手动打开权限按钮
+                openAppDetails();
+            }
+        }
+    }
+
+    /**
+     * 第 4 步: 备份通讯录操作
+     */
+    private void doBackup() {
+        // 本文主旨是讲解如果动态申请权限, 具体备份代码不再展示, 就假装备份一下
+        Toast.makeText(this, "正在将图片转为avi...", Toast.LENGTH_SHORT).show();
+
+        test();
+
+        new EncodeAndMuxTest().testEncodeVideoToMp4();
+    }
+
+    /**
+     * 打开 APP 的详情设置
+     */
+    private void openAppDetails() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("文件存储需要权限，请到 “应用信息 -> 权限” 中授予！");
+        builder.setPositiveButton("去手动授权", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("取消", null);
+        builder.show();
+    }
+
+    private void test() {
+        String outputPath = new File(OUTPUT_DIR,
+                "test.avi").toString();
+        Log.i("test", "-outputPath=" + outputPath);
+        for (int i = 0; i < images.length; i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), images[i]);
+            ImageVideoConverter.convertImageToVideo(bitmap, 6, 1024, 760, outputPath);
+            Log.i("test", "-i=" + i);
+        }
+    }
+
+    /**
+     * 检查是否拥有指定的所有权限
+     */
+    private boolean checkPermissionAllGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                // 只要有一个权限没有被授予, 则直接返回 false
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 获取图片的存储路径
+     *
+     * @param context 上下文
+     * @return 手机可存储路径
+     */
+    public static String getImagePath(Context context) {
+        return getRootPath(context) + "/POCUS/image/";
+    }
+
+    /**
+     * 获取手机可存储路径
+     *
+     * @param context 上下文
+     * @return 手机可存储路径
+     */
+    public static String getRootPath(Context context) {
+        // 是否有SD卡,包含可拔插和不可拔插
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)
+                || !Environment.isExternalStorageRemovable()) {
+            // 获取SD卡根目录
+            return Environment.getExternalStorageDirectory().getPath();
+        } else {
+            // 获取apk包下的缓存路径
+            return context.getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath();
+        }
+    }
+
+}
